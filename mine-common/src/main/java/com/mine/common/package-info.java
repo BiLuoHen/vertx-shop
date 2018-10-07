eccfaf7a444fdcf7fd622e0a477a464a0d376447
BiLuoHen <biluohen@gmail.com>
2018-10-08 00:51:30 +0800

    common

---
+++ mine-common/src/main/java/com/mine/common/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Duhuafei on 10/7/2018.
+ */
+@ModuleGen(name = "mine-common", groupPackage = "com.mine.common")
+package com.mine.common;
+
+import io.vertx.codegen.annotations.ModuleGen;
